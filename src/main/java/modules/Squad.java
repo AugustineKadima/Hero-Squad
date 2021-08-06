4c7c43e52771e92f14368891055b94d5e6cf17ec
C0d3president <kadimasam@gmail.com>
2021-08-07 00:59:22 +0300

    Generate getter and setter for the squad class

--- src/main/java/modules/Squad.java
+++ src/main/java/modules/Squad.java
@@ -1,8 +1,42 @@
 package modules;
 
 public class Squad {
+
     private String squadName;
     private String[] squadMembers = new String[5];
     private String cause;
     private int id;
+
+    public String getSquadName() {
+        return squadName;
+    }
+
+    public void setSquadName(String squadName) {
+        this.squadName = squadName;
+    }
+
+
+    public String[] getSquadMembers() {
+        return squadMembers;
+    }
+
+    public void setSquadMembers(String[] squadMembers) {
+        this.squadMembers = squadMembers;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
