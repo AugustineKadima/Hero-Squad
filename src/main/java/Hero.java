8d2dbc24181007539501b0894f961c1f85fa2e2f
C0d3president <kadimasam@gmail.com>
2021-08-06 18:28:41 +0300

    Generate getter and setter methods for the hero class

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,8 +1,40 @@
 public class Hero {
-    String heroName;
-    String superPower;
-    String weakness;
-    int age;
 
-    
+    private String heroName;
+    private String superPower;
+    private String weakness;
+    private int age;
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public String getSuperPower() {
+        return superPower;
+    }
+
+    public void setSuperPower(String superPower) {
+        this.superPower = superPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
